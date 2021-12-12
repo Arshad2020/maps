790f63338913d39bb9696b4a1c3835a7238d4ae0
Arshad <arshad.ibrahim@polestar.com>
2021-12-12 08:27:13 +0100

    firt commit for deploy

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+/// <reference types="@types/google.maps" />
+import {User} from "./User"
+import {Company} from "./Company"
+import {CustomMap} from "./CustomMap"
+
+
+
+const user = new User();
+
+const customMap = new CustomMap("map")
+
+customMap.addMarker(user)
+
+const company = new Company()
+
+customMap.addMarker(company)
+
