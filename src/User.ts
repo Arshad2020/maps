528efcccfef034939056325d1ea277ab4ab231a8
Arshad <arshad.ibrahim@polestar.com>
2022-01-10 13:24:18 +0100

    First commit

--- src/User.ts
+++ src/User.ts
@@ -1,21 +1,23 @@
-import faker from 'faker'
-import {Mappable} from "./CustomMap"
-export class User implements Mappable{
+import * as faker from "faker";
+import { Mappable } from './CustomMap';
+
+export class User implements Mappable {
   name: string;
-  location:{
+  location: {
     lat: number;
-    lng:number
-  }
-  constructor(){
+    lng: number;
+  };
+  color: string = 'red';
+
+  constructor() {
     this.name = faker.name.firstName();
-    this.location ={
-      lat:parseFloat(faker.address.latitude()),
-      lng:parseFloat(faker.address.longitude())
-    }
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    };
   }
 
-  markerContent():string{
-    return `User Name: ${this.name}`
+  markerContent(): string {
+    return `User Name: ${this.name}`;
   }
 }
-
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,10 @@
-/// <reference types="@types/google.maps" />
-import {User} from "./User"
-import {Company} from "./Company"
-import {CustomMap} from "./CustomMap"
-
-
+import { User } from './User';
+import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
 const user = new User();
+const company = new Company();
+const customMap = new CustomMap('map');
 
-const customMap = new CustomMap("map")
-
-customMap.addMarker(user)
-
-const company = new Company()
-
-customMap.addMarker(company)
-
+customMap.addMarker(user);
+customMap.addMarker(company);
